3f3c35efe470ac3dc285c9e9df09e5f4665ed5c0
Abbas Ali <abbasali5784@gmail.com>
2023-09-01 18:31:56 -0400

    Added disappearing keyboard when network connection is gone

--- components/Chat.js
+++ components/Chat.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { StyleSheet, View, Text, Alert, Keyboard } from "react-native";
 import {
   Bubble,
@@ -30,23 +30,32 @@ const Chat = ({ db, route, navigation, isConnected }) => {
   }, [isConnected]);
 
   useEffect(() => {
+    let isMounted = true;
     if (isConnected === true) {
-      const q = query(collection(db, "messages"), where("uid", "==", userId));
-      unsubMessages = onSnapshot(q, (documentsSnapshot) => {
+      const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+      const unsubMessages = onSnapshot(q, (documentsSnapshot) => {
         let newMessages = [];
         documentsSnapshot.forEach((doc) => {
-          newMessages.push({ id: doc.id, ...doc.data() });
+          const data = doc.data();
+          newMessages.push({
+            _id: doc.id,
+            ...data,
+            createdAt: new Date(data.createdAt),
+          });
         });
-        cachedMessages(newMessages);
-        setMessages(newMessages);
-      });
-    } else loadCachedMessages();
 
-    return () => {
-      if (unsubMessages) unsubMessages();
-      unsubMessages = null;
-    };
-  }, [isConnected]);
+        cachedMessages(newMessages);
+        if (isMounted) {
+          setMessages(newMessages);
+        }
+      });
+
+      return () => {
+        isMounted = false;
+        if (unsubMessages) unsubMessages();
+      };
+    } else loadCachedMessages();
+  }, [isConnected, loadCachedMessages]);
 
   useEffect(() => {
     navigation.setOptions({ title: name });
@@ -60,32 +69,41 @@ const Chat = ({ db, route, navigation, isConnected }) => {
     }
   };
 
-  const loadCachedMessages = async () => {
+  const loadCachedMessages = useCallback(async () => {
+    let isMounted = true;
     try {
       const cachedMessages = await AsyncStorage.getItem("messages");
-      if (cachedMessages !== null) {
+      if (cachedMessages !== null && isMounted) {
         setMessages(JSON.parse(cachedMessages));
       }
     } catch (error) {
       console.error(error);
     }
-  };
+    return () => {
+      isMounted = false;
+    };
+  }, []);
 
   useEffect(() => {
     loadCachedMessages();
-  }, []);
+  }, [loadCachedMessages]);
 
   const onSend = async (newMessages) => {
     if (!isConnected) {
       Alert.alert("You are offline. Messages cannot be sent.");
       return;
     }
-    const newMessage = { ...newMessages[0], uid: userId };
+    const newMessage = {
+      ...newMessages[0],
+      uid: userId,
+      createdAt: new Date().toISOString(),
+      user: {
+        _id: userId,
+        name: name,
+      },
+    };
     try {
       await addDoc(collection(db, "messages"), newMessage);
-      setMessages((previousMessages) =>
-        GiftedChat.append(previousMessages, [newMessage])
-      );
     } catch (error) {
       console.error("Error adding document: ", error);
     }
